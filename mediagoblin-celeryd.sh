5455c6d6760989da25fbe3c59601a57a8d8130c5
Joar Wandborg <git@wandborg.com>
2012-05-23 22:09:46 +0200

    Added mediagoblin-celery, fixed som things in mediagoblin-paster

--- mediagoblin-celeryd.sh
+++ mediagoblin-celeryd.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# /etc/init.d/mediagoblin-paster
+# /etc/init.d/mediagoblin-celeryd
 #
 ## LICENSE: CC0 <http://creativecommons.org/publicdomain/zero/1.0/>
 # To the extent possible under law, Joar Wandborg <http://wandborg.se> has
 # waived all copyright and related or neighboring rights to
-# mediagoblin-paster. This work is published from Sweden.
+# mediagoblin-celeryd. This work is published from Sweden.
 #
 ## CREDIT
 # Credit goes to jpope <http://jpope.org/> and 
@@ -12,15 +12,15 @@
 # based upon.
 #
 ### BEGIN INIT INFO
-# Provides:          mediagoblin-paster
+# Provides:          mediagoblin-celeryd
 # Required-Start:    $network $named $local_fs
 # Required-Stop:     $remote_fs $syslog $network $named $local_fs
-# Should-Start:      postgresql $syslog
+# Should-Start:      postgres $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: MediaGoblin paster FCGI server init script
-# Description:       This script will initiate the GNU MediaGoblin paster
-#                    fcgi server.
+# Short-Description: MediaGoblin Celery task processor init script
+# Description:       This script will initiate the GNU MediaGoblin Celery 
+#                    task processor 
 ### END INIT INFO
 
 ################################################################################
@@ -35,16 +35,14 @@ MG_ROOT=/home/joar/git/mediagoblin
 
 set -e
 
-DAEMON_NAME=mediagoblin-paster
+DAEMON_NAME=mediagoblin-celeryd
 
-MG_BIN=$MG_ROOT/bin
-MG_PASTER_BIN=$MG_BIN/paster
-MG_PASTE_INI=$MG_ROOT/paste_local.ini
 MG_USER=joar
-MG_FCGI_HOST=127.0.0.1
-MG_FCGI_PORT=26543
-MG_PASTER_PID_FILE=/var/run/mediagoblin/$DAEMON_NAME.pid
-MG_PASTER_LOG_FILE=/var/log/mediagoblin/$DAEMON_NAME.log
+MG_BIN=$MG_ROOT/bin
+MG_CELERYD_BIN=$MG_BIN/celeryd
+MG_CELERY_CONFIG_MODULE=mediagoblin.init.celery.from_celery
+MG_CELERYD_PID_FILE=/var/run/mediagoblin/$DAEMON_NAME.pid
+MG_CELERYD_LOG_FILE=/var/log/mediagoblin/$DAEMON_NAME.log
 
 set_up_directories() {
     install -o $MG_USER -g users -d -m 755 /var/log/mediagoblin
@@ -56,44 +54,43 @@ set_up_directories
 # Include LSB helper functions
 . /lib/lsb/init-functions
 
-getPID () {
+getPID() {
     # Discard any errors from cat
-    cat $MG_PASTER_PID_FILE 2>/dev/null
+    cat $MG_CELERYD_PID_FILE 2>/dev/null
 }
 
 case "$1" in 
     start)
-        # Start the MediaGoblin paster process
-        log_daemon_msg "Starting GNU MediaGoblin paster fcgi server" "$DAEMON_NAME"
+        # Start the MediaGoblin celeryd process
+        log_daemon_msg "Starting GNU MediaGoblin Celery task queue" "$DAEMON_NAME"
         if [ -z "$(getPID)" ]; then
-            su -s /bin/sh -c "CELERY_ALWAYS_EAGER=false ${MG_PASTER_BIN} serve \
-                $MG_PASTE_INI \
-                --server-name=fcgi \
-                fcgi_host=$MG_FCGI_HOST fcgi_port=$MG_FCGI_PORT \
-                --pid-file=$MG_PASTER_PID_FILE \
-                --log-file=$MG_PASTER_LOG_FILE \
-                --daemon" - $MG_USER 2>&1 > /dev/null
+            su -s /bin/sh -c "cd $MG_ROOT && \
+                CELERY_CONFIG_MODULE=$MG_CELERY_CONFIG_MODULE \
+                $MG_CELERYD_BIN \
+                --pidfile=$MG_CELERYD_PID_FILE \
+                -f $MG_CELERYD_LOG_FILE \
+                --loglevel=INFO" - $MG_USER 2>&1 > /dev/null &
 
-            PASTER_RESULT=$?
-
-            # Sleep for a while until we're kind of certain that paster has
+            CELERYD_RESULT=$?
+            
+            # Sleep for a while until we're kind of certain that celeryd has
             # had it's time to initialize
             TRIES=0
-            while ! [ "X$PASTER_RESULT" != "X" ]; do
+            while ! [ "X$CELERYD_RESULT" != "X" ]; do
                 log_action_msg "Tried $TRIES time(s)"
                 sleep 0.1
                 TRIES=$((TRIES+1))
             done
 
-            log_end_msg $PASTER_RESULT
+            log_end_msg $CELERYD_RESULT
         else
             # Failed because the PID file indicates it's running
-            log_action_msg "PID file $MG_PASTER_BIN already exists"
+            log_action_msg "PID file $MG_CELERYD_BIN already exists"
             log_end_msg 1
         fi
         ;;
     stop)
-        log_daemon_msg "Stopping GNU MediaGoblin paster fcgi server" "$DAEMON_NAME"
+        log_daemon_msg "Stopping GNU MediaGoblin Celery task queue" "$DAEMON_NAME"
         if [ -z "$(getPID)" ]; then
             # Failed because the PID file indicates it's not running
             RET=1
